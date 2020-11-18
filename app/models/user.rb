9398f2de929bde9a7007d253cdad5d50f434dd39
Кот Miseinen <miseinen22@gmail.com>
2020-11-18 19:36:19 +0300

    Add before_save action for email to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  
+  before_save { self.email = email.downcase }
+
   has_many :articles
 
   validates :username, presence: true,
@@ -7,8 +8,8 @@ class User < ApplicationRecord
                        length: 3..25
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, 
-                    uniqueness: { case_sensitive: false },
+  validates :email, presence: true,
+git                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX },
                     length: { maximum: 105 }
 end
