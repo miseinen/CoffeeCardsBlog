72e774042d3ced163fc3c7216fd23d2053c73be9
Кот Miseinen <miseinen22@gmail.com>
2020-11-21 20:37:11 +0300

    Add current_user and logged_in? helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       .select { |v| v.class == ActiveRecord::Validations::LengthValidator }
       .select { |v| v.options[:maximum].present? }.first.options[:maximum]
   end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
